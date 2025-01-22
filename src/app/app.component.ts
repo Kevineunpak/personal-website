5e136a97d8e5700a91fd9d43cac998b1247b92cd
kevineunpak <kevineunpak@gmail.com>
2025-01-22 22:59:33 +0900

    Home Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router'; // Import Router and NavigationEnd
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './Components/header/header.component'; // Adjust path as needed
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { HeaderComponent } from './Components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, HeaderComponent], // Add HeaderComponent here
+  imports: [RouterModule, CommonModule, HeaderComponent], // Add CommonModule here
 })
-export class AppComponent {}
+export class AppComponent {
+  showHeader = true; // Default to true
+
+  constructor(private router: Router) {
+    // Listen to route changes
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current route is the homepage
+        this.showHeader = event.urlAfterRedirects !== '/';
+      }
+    });
+  }
+}
